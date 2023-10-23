bc1c565e814f7391caedec30df948a277c247c31
Arishavelle Karl Villanueva <59736975+arishavelle18@users.noreply.github.com>
2023-10-23 13:51:06 +0800

    Add project files.

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using WeatherAppAssignment.Models;
+
+namespace WeatherAppAssignment.Controllers;
+
+public class WeatherController : Controller
+{
+    [Route("/")]
+    public IActionResult Index()
+    {
+        List<CityWeather> weatherData = new List<CityWeather>()
+        {
+            new CityWeather {CityUniqueCode = "LDN", CityName = "London", DateAndTime = DateTime.Parse("2030-01-01 8:00"),  TemperatureFahrenheit = 33},
+            new CityWeather {CityUniqueCode = "NYC", CityName = "London", DateAndTime = DateTime.Parse("2030-01-01 3:00"),  TemperatureFahrenheit = 60},
+            new CityWeather {CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = DateTime.Parse("2030-01-01 9:00"),  TemperatureFahrenheit = 82}
+        };
+        return View(weatherData);
+    }
+
+    [Route("/weather/{cityCode}")]
+    public IActionResult ViewDetails(string? cityCode)
+    {
+        List<CityWeather> weatherData = new List<CityWeather>()
+        {
+            new CityWeather {CityUniqueCode = "LDN", CityName = "London", DateAndTime = DateTime.Parse("2030-01-01 8:00"),  TemperatureFahrenheit = 33},
+            new CityWeather {CityUniqueCode = "NYC", CityName = "London", DateAndTime = DateTime.Parse("2030-01-01 3:00"),  TemperatureFahrenheit = 60},
+            new CityWeather {CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = DateTime.Parse("2030-01-01 9:00"),  TemperatureFahrenheit = 82}
+        };
+
+        CityWeather? mathchingCity = weatherData.Where(city => city.CityUniqueCode == cityCode).FirstOrDefault();
+
+        return View(mathchingCity);
+    }
+}
+
