533b80e3d0e5d00dfb54e8c685a2dfa092c69d67
Arishavelle Karl Villanueva <59736975+arishavelle18@users.noreply.github.com>
2023-11-28 23:28:21 +0800

    Nov282023-ApplyViewComponent

---
+++ ViewComponents/WeatherGridViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAppAssignment.Models;
+
+namespace WeatherAppAssignment.ViewComponents;
+
+public class WeatherGridViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(List<CityWeather> grid)
+    {
+        return View("Sample");
+    }
+}
+
