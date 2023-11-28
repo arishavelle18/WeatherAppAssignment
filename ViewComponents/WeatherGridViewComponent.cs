2b0047407302a3c29f1b20a24690924410080ecc
Arishavelle Karl Villanueva <59736975+arishavelle18@users.noreply.github.com>
2023-11-28 23:49:53 +0800

    Nov28-2023-RefactorUsingComponent

--- ViewComponents/WeatherGridViewComponent.cs
+++ ViewComponents/WeatherGridViewComponent.cs
@@ -5,9 +5,24 @@ namespace WeatherAppAssignment.ViewComponents;
 
 public class WeatherGridViewComponent : ViewComponent
 {
-    public async Task<IViewComponentResult> InvokeAsync(List<CityWeather> grid)
+    public async Task<IViewComponentResult> InvokeAsync(CityWeather city)
     {
-        return View("Sample");
+        ViewData["TemperatureFahrenheit"] = GetCssClassByFahrenheit(city.TemperatureFahrenheit);
+        return View("Sample",city);
+    }
+    public string GetCssClassByFahrenheit(int temp){
+        if (temp < 44)
+        {
+            return "blue-back";
+        }
+        else if (temp >= 44 && temp < 74)
+        {
+            return "green-back";
+        }
+        else
+        {
+            return "orange-back";
+        }
     }
 }
 
